a54dc02632089e4933402ff02d98acc3cd850d3c
Alex Necakov <alexnecakov@gmail.com>
2024-08-11 18:49:06 -0400

    refactored charlist to gamelist

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-let charlist = []
+let gamelist = []
 
 function shuffle(array) {
   let currentIndex = array.length,
@@ -53,15 +53,15 @@ function hideAll() {
   document.getElementById('resumeButton').style.display = 'none';
 }
 async function initialize() {
-  if (window.localStorage['charlist']) {
+  if (window.localStorage['gamelist']) {
     if (!window.confirm('Saved progress detected. Are you sure you want to start over?')) {
       return;
     }
   }
   let keys = await Object.keys(library)
-  charlist = charlist.concat(keys)
-  if (charlist.length < 2) {
-    window.alert('You need to select more than 2 characters.')
+  gamelist = gamelist.concat(keys)
+  if (gamelist.length < 2) {
+    window.alert('You need to provide more than 2 games.')
     return;
   }
   this.hideAll()

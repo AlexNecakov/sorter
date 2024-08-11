d3e352b252761281594c504b9ff5e95d9f6422fb
Alex Necakov <alexnecakov@gmail.com>
2024-08-11 19:21:49 -0400

    added most recent results

--- scripts/library.js
+++ scripts/library.js
@@ -1,8 +1,242 @@
 let library = {
+    "darkestdungeon": {
+        "display": "Darkest Dungeon"
+    },
+    "finalfantasyii": {
+        "display": "Final Fantasy II"
+    },
+    "dragonquest1": {
+        "display": "Dragon Quest 1"
+    },
+    "cryptofthenecrodancer": {
+        "display": "Crypt of the Necrodancer"
+    },
+    "chronotrigger": {
+        "display": "Chrono Trigger"
+    },
+    "demon'ssouls": {
+        "display": "Demon's Souls"
+    },
+    "bloodborne": {
+        "display": "Bloodborne"
+    },
+    "bioshock": {
+        "display": "Bioshock"
+    },
+    "baldur'sgate": {
+        "display": "Baldur's Gate"
+    },
+    "batmanarkhamasylum": {
+        "display": "Batman Arkham Asylum"
+    },
+    "finalfantasyviii": {
+        "display": "Final Fantasy VIII"
+    },
+    "castlevaniacircleofthemoon": {
+        "display": "Castlevania: Circle of the Moon"
+    },
+    "supermariorpg": {
+        "display": "Super Mario RPG"
+    },
+    "papermario": {
+        "display": "Paper Mario"
+    },
+    "papermariothethousandyeardoor": {
+        "display": "Paper Mario The Thousand Year Door"
+    },
+    "mario&luigisuperstarsaga": {
+        "display": "Mario & Luigi: Superstar Saga"
+    },
+    "indivisible": {
+        "display": "Indivisible"
+    },
+    "supermariobros2": {
+        "display": "Super Mario Bros 2"
+    },
+    "supermariobros3": {
+        "display": "Super Mario Bros 3"
+    },
+    "yoshi'sisland": {
+        "display": "Yoshi's Island"
+    },
+    "castlevaniadawnofsorrow": {
+        "display": "Castlevania: Dawn of Sorrow"
+    },
+    "cuphead": {
+        "display": "Cuphead"
+    },
+    "finalfantasyx": {
+        "display": "Final Fantasy X"
+    },
+    "megaman3": {
+        "display": "Mega Man 3"
+    },
+    "hollowknight": {
+        "display": "Hollow Knight"
+    },
+    "hitman2016": {
+        "display": "Hitman (2016)"
+    },
+    "cavestory": {
+        "display": "Cave Story"
+    },
+    "specopstheline": {
+        "display": "Spec Ops the Line"
+    },
+    "xcomenemyunknown": {
+        "display": "XCOM Enemy Unknown"
+    },
+    "diceydungeons": {
+        "display": "Dicey Dungeons"
+    },
     "fallout": {
         "display": "Fallout"
     },
-    "baldursgate": {
-        "display": "Baldur's Gate"
+    "sekiro": {
+        "display": "Sekiro"
+    },
+    "wolfensteintheneworder": {
+        "display": "Wolfenstein The New Order"
+    },
+    "oblivion": {
+        "display": "Oblivion"
+    },
+    "doom": {
+        "display": "Doom"
+    },
+    "daggerfall": {
+        "display": "Daggerfall"
+    },
+    "newsupermariobroswii": {
+        "display": "New Super Mario Bros Wii"
+    },
+    "supermario3dworld": {
+        "display": "Super Mario 3D World"
+    },
+    "riseofthetombraider": {
+        "display": "Rise of the Tomb Raider"
+    },
+    "residentevilremake": {
+        "display": "Resident Evil Remake"
+    },
+    "oriandtheblindforest": {
+        "display": "Ori and the Blind Forest"
+    },
+    "marvel'sspider-man": {
+        "display": "Marvel's Spider-Man"
+    },
+    "shadowofmordor": {
+        "display": "Shadow of Mordor"
+    },
+    "thewitcher": {
+        "display": "The Witcher"
+    },
+    "kotor": {
+        "display": "KotOR"
+    },
+    "elderscrollsarena": {
+        "display": "Elder Scrolls Arena"
+    },
+    "sonicadventuredx": {
+        "display": "Sonic Adventure DX"
+    },
+    "dishonored": {
+        "display": "Dishonored"
+    },
+    "spyrotrilogy": {
+        "display": "Spyro Trilogy"
+    },
+    "devilmaycry": {
+        "display": "Devil May Cry"
+    },
+    "deusexhumanrevolution": {
+        "display": "Deus Ex Human Revolution"
+    },
+    "crashbandicoottrilogy": {
+        "display": "Crash Bandicoot Trilogy"
+    },
+    "bioshockinfinite": {
+        "display": "Bioshock Infinite"
+    },
+    "amnesia": {
+        "display": "Amnesia"
+    },
+    "blackmesa": {
+        "display": "Black Mesa"
+    },
+    "deadcells": {
+        "display": "Dead Cells"
+    },
+    "banjo-kazooie": {
+        "display": "Banjo-Kazooie"
+    },
+    "persona4golden": {
+        "display": "Persona 4 Golden"
+    },
+    "fireemblemmysteryoftheemblem": {
+        "display": "Fire Emblem Mystery of the Emblem"
+    },
+    "oracleofages": {
+        "display": "Oracle of Ages"
+    },
+    "deusex": {
+        "display": "Deus Ex"
+    },
+    "pokemongen2": {
+        "display": "Pokemon Gen 2"
+    },
+    "pikmin": {
+        "display": "Pikmin"
+    },
+    "dokaponkingdom": {
+        "display": "Dokapon Kingdom"
+    },
+    "godofwarii": {
+        "display": "God of War II"
+    },
+    "persona3fes": {
+        "display": "Persona 3 FES"
+    },
+    "ico": {
+        "display": "Ico"
+    },
+    "residentevil3": {
+        "display": "Resident Evil 3"
+    },
+    "zeldaii": {
+        "display": "Zelda II"
+    },
+    "fireemblemthreehouses": {
+        "display": "Fire Emblem Three Houses"
+    },
+    "miketyson'spunch-out": {
+        "display": "Mike Tyson's Punch-Out"
+    },
+    "luigi'smansion": {
+        "display": "Luigi's Mansion"
+    },
+    "fourswordsadventures": {
+        "display": "Four Swords Adventures"
+    },
+    "kidicarus": {
+        "display": "Kid Icarus"
+    },
+    "fireemblemgaiden": {
+        "display": "Fire Emblem Gaiden"
+    },
+    "triforceheroes": {
+        "display": "Triforce Heroes"
+    },
+    "deathstranding": {
+        "display": "Death Stranding"
+    },
+    "donkeykong64": {
+        "display": "Donkey Kong 64"
+    },
+    "darkcloud": {
+        "display": "Dark Cloud"
+    },
+    "castlevaniadraculax": {
+        "display": "Castlevania: Dracula X"
     }
-}
+};
